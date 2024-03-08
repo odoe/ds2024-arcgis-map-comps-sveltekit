5e789795bcbb20d1b120059d2fda07d590ac8830
Rene Rubalcava <rrubalcava@esri.com>
2024-03-08 12:59:05 -0800

    chore: fix typing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,46 @@
 <script lang="ts">
-	import '@arcgis/core/assets/esri/themes/dark/main.css';
-	import './../index.css';
+  import '@arcgis/core/assets/esri/themes/dark/main.css';
+  import './../index.css';
 
-	import { defineCustomElements } from '@arcgis/map-components/dist/loader';
+  import { defineCustomElements } from '@arcgis/map-components/dist/loader';
 
-	defineCustomElements();
+  import type { ArcgisMapCustomEvent } from '@arcgis/map-components';
 
-	function handleMapLoad(e: unknown) {
-		console.log('Map loaded', e);
-	}
+  defineCustomElements();
 
-	const popup = {
-		dockEnabled: true,
-		dockOptions: {
-			position: 'bottom-right',
-			breakpoint: false
-		}
-	};
+  function handleMapLoad(e: ArcgisMapCustomEvent<void>) {
+    console.log('Map loaded', e);
+  }
 
-	const theme = {
-		accentColor: 'purple',
-		textColor: [125, 255, 13, 0.9]
-	};
+  const popup = {
+    dockEnabled: true,
+    dockOptions: {
+      position: 'bottom-right',
+      breakpoint: false
+    }
+  };
+
+  const theme = {
+    accentColor: 'purple',
+    textColor: [125, 255, 13, 0.9]
+  };
 </script>
 
 <arcgis-map
-	item-id="d5dda743788a4b0688fe48f43ae7beb9"
-	on:arcgisViewReadyChange={handleMapLoad}
-	{popup}
-	{theme}
+  item-id="d5dda743788a4b0688fe48f43ae7beb9"
+  on:arcgisViewReadyChange={handleMapLoad}
+  {popup}
+  {theme}
 >
-	<arcgis-search position="top-right" />
-	<arcgis-expand position="bottom-left">
-		<arcgis-area-measurement-2d />
-	</arcgis-expand>
+  <arcgis-search position="top-right" />
+  <arcgis-expand position="bottom-left">
+    <arcgis-area-measurement-2d />
+  </arcgis-expand>
 </arcgis-map>
 
 <style>
-	arcgis-map {
-		height: 100%;
-		width: 100%;
-	}
+  arcgis-map {
+    height: 100%;
+    width: 100%;
+  }
 </style>
